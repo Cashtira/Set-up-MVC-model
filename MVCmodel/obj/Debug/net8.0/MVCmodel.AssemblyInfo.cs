abbc5e4338f0aeb4a8f8f7762ba3c6165e993b41
Cashtira <22520599@gm.uit.edu.vn>
2024-09-24 14:19:50 +0700

    Download Entity Framework

--- MVCmodel/obj/Debug/net8.0/MVCmodel.AssemblyInfo.cs
+++ MVCmodel/obj/Debug/net8.0/MVCmodel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCmodel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b325354c38745461ebc61bf749deba07cf45629")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCmodel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCmodel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
